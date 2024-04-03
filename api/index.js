bc139c5b4d2b80ca498cc06efb1c72d00b55f3b2
SKHanzla <hanzlaonmail12@gmail.com>
2024-04-03 02:59:30 -0700

    initial commit

---
+++ api/index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import routes from './src/routes/crmRoutes';
+
+const app = express();
+const PORT = 4002;
+
+// mongoose connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb+srv://Hanzla-Tariq:Shooterin%4012@cluster0.phsr1.mongodb.net/Hanzla').then(
+    console.log("I am running")
+);
+
+// bodyparser setup
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+routes(app);
+
+// serving static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.send(`Node and express server is running on port ${PORT}`)
+);
+
+app.listen(PORT, () =>
+    console.log(`your server is running on port ${PORT}`)
+);
